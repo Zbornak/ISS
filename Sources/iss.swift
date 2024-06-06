73ec288ec06cce380a9555d89ba8e78f0b68b88a
zbornak <zbornak@protonmail.com>
2024-06-06 15:59:42 +0100

    Added km/s to speed

--- Sources/iss.swift
+++ Sources/iss.swift
@@ -84,6 +84,6 @@ func getSpeed() {
     let data: SatelliteData = interpreter.satelliteData(from: tle, date: .now)
     
     let formattedSpeed = String(format: "%.2f", data.speed)
-    print("The ISS is currently travelling at a speed of \(formattedSpeed) km/h.")
+    print("The ISS is currently travelling at a speed of \(formattedSpeed) km/h, or \((Float(formattedSpeed) ?? 0) * 0.0002777778) km/s.")
 }
 
