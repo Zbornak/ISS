102b0ee82fe87c2ff9fe181bc0baffdc714f88e7
zbornak <zbornak@protonmail.com>
2024-06-06 13:44:42 +0100

    Retrieving iss coordinates from NORAD

--- Sources/iss.swift
+++ Sources/iss.swift
@@ -8,14 +8,40 @@
 import ArgumentParser
 import Figlet
 import Foundation
-
-let parser = HTMLParser()
+import SwiftSoup
 
 @main
 struct Iss: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "A cli which lets you know the current position of the International Space Station, as well as who is on board and other interesting random trivia", version: "1.0.0")
     
     mutating func run() throws {
-        print(parser.parse(from: "https://www.astroviewer.net/iss/en/"))
+        print(HTMLParse(from: "http://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE"))
     }
+}
+
+func HTMLParse(from: String) -> String {
+    var contents = ""
+    
+    if let url = URL(string: from) {
+        do {
+            contents = try String(contentsOf: url)
+        } catch {
+            print("contents could not be loaded")
+        }
+    } else {
+        print("Bad URL")
+    }
+    
+    do {
+        let document: Document = try SwiftSoup.parse(contents)
+    } catch {
+        print("HTML not found")
+    }
+    
+    return contents
+}
+
+func getTLE() -> [String] {
+    
+    return [""]
 }
