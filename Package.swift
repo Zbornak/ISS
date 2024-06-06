e9f9732399de99ce18005d56b89b1621231371a2
zbornak <zbornak@protonmail.com>
2024-06-06 14:45:06 +0100

    Now tracking ISS based on NORAD data

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0")
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
+        .package(url: "https://github.com/csanfilippo/swift-sgp4", from: "1.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -18,7 +19,8 @@ let package = Package(
         dependencies: [
             .product(name: "Figlet", package: "example-package-figlet"),
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            .byName(name: "SwiftSoup")
+            .byName(name: "SwiftSoup"),
+            .product(name: "SGPKit", package: "swift-sgp4")
         ],
         path: "Sources"),
     ]
--- Sources/iss.swift
+++ Sources/iss.swift
@@ -8,14 +8,16 @@
 import ArgumentParser
 import Figlet
 import Foundation
+import SGPKit
 import SwiftSoup
 
+@available(macOS 12, *)
 @main
 struct Iss: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "A cli which lets you know the current position of the International Space Station, as well as who is on board and other interesting random trivia", version: "1.0.0")
     
     mutating func run() throws {
-        print("Hello")
+        getCoords()
     }
 }
 
@@ -45,4 +47,21 @@ func getTLE() -> [String] {
     let rawString = HTMLParse(from: "http://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE")
     let newArray = rawString.components(separatedBy: "\r\n")
     return newArray
+}
+
+@available(macOS 12, *)
+func getCoords() {
+    let dataArray = getTLE()
+    let title = dataArray[0]
+    let firstLine = dataArray[1]
+    let secondLine = dataArray[2]
+    
+    let tle = TLE(title: title, firstLine: firstLine, secondLine: secondLine)
+    let interpreter = TLEInterpreter()
+    
+    let data: SatelliteData = interpreter.satelliteData(from: tle, date: .now)
+    
+    // print(data.altitude)
+    // print(data.speed)
+    print("The ISS is currently at latitude: \(data.latitude), longitude: \(data.longitude)")
 }
